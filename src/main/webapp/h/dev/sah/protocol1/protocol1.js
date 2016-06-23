9d91333ba119fdfec5475f790a7041e7ac7d1209
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2016-06-24 02:02:56 +0300

    read write to json file

--- src/main/webapp/h/dev/sah/protocol1/protocol1.js
+++ src/main/webapp/h/dev/sah/protocol1/protocol1.js
@@ -1,9 +1,23 @@
 var myAppModule = angular.module('MyApp', [ 'ng.jsoneditor' ]);
-angular .module('MyApp', [ 'ng.jsoneditor' ]) .controller( 'ngCtrl', function($scope) {
+angular .module('MyApp', [ 'ng.jsoneditor' ]) .controller( 'ngCtrl', function($scope, $http) {
 	$scope.obj = {
-		data : protocol1,
-		options : { mode : 'tree' }
+			data : {},
+			options : { mode : 'tree' }
 	};
+	$http.get("/v/readContent").success(function(response) {
+		var protocol1 = response;
+		console.log(protocol1);
+		$scope.obj = {
+			data : protocol1,
+			options : { mode : 'tree' }
+		};
+	});
+	$scope.saveFile = function(){
+		console.log("-------saveFile--------");
+		$http.post("/saveCommonContent", $scope.obj.data ).success(function(response) {
+			console.log(response);
+		});
+	}
 	var notShowFields = ["xmlContent", "openChild", "editField"];
 	$scope.isShowField = function(key) {
 		return !(notShowFields.indexOf(key) >= 0);
@@ -16,7 +30,10 @@ angular .module('MyApp', [ 'ng.jsoneditor' ]) .controller( 'ngCtrl', function($s
 		console.log(object);
 	};
 	$scope.editField = function(object, fieldName) {
-		object.editField = fieldName;
+		if(object.editField == fieldName)
+			object.editField = "";
+		else
+			object.editField = fieldName;
 	};
 	$scope.isObject = function(object) {
 		return angular.isObject(object);
